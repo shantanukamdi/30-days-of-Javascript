68a7cfaa908145348d1239a423d6da7b0139f4aa
Shantanu Kamdi <kamdishantanu@gmail.com>
2018-07-26 07:57:25 +0530

    day 9 completed

---
+++ day9/main.js
@@ -0,0 +1,25 @@
+const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]');
+
+// console.log(checkboxes);
+let lastChecked;
+
+checkboxes.forEach(checkbox => {
+    checkbox.addEventListener('click', handleCheck);
+});
+
+function handleCheck(e) {
+    let inBetween = false;
+    if(e.shiftKey && this.checked) {
+        checkboxes.forEach(checkbox => {
+            // console.log(checkbox);
+            if(checkbox === this || checkbox === lastChecked) {
+                inBetween = !inBetween;
+                console.info('Starting to check inBetween');
+            }
+            if(inBetween) {
+                checkbox.checked = true;
+            }
+        });
+    }
+    lastChecked = this;
+}
